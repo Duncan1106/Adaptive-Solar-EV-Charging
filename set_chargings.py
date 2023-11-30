2c6f45344f81c6d6a62ce8444b1aa7a427311743
Duncan1106 <duncan.zirkel@gmail.com>
2023-11-30 22:06:30 +0100

    imporved code

--- set_chargings.py
+++ set_chargings.py
@@ -1,18 +1,17 @@
-from requests import get
+import requests
 
 def set_charging(s: int) -> bool:
     """Send a GET request to start or stop charging the EV.
 
     Args:
-        s (int): The status to set for charging. 0 to start charging, 1 to stop charging.
+        s (str): The status to set for charging. 0 to start charging, 1 to stop charging.
 
     Returns:
         bool: True if the GET request was successful and the status code is 200, False otherwise.
     """
     set_charging_url = "http://192.168.2.203/api/set?frc="
-    r = get(set_charging_url + str(s))
+    r = requests.get(set_charging_url + str(s))
     return r.status_code == 200
-
 
 # check if script is run as a script
 if __name__ == "__main__":
