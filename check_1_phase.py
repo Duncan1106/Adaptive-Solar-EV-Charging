d2286a299d3dc5b527f86a832be78b168b5671eb
Duncan1106 <duncan.zirkel@gmail.com>
2023-11-30 21:58:49 +0100

    removed three phase changing due to bugs with charger and reset of rfid chip on switch of phases used modbus for faster data from solar WR configure logging before running the progress bar sleep and not exit if not using 3 phases after firstly using it

--- check_1_phase.py
+++ check_1_phase.py
@@ -1,20 +1,20 @@
-import sys ### possibly not needed
 from time import sleep
 from requests import get
+from datetime import datetime
+from solar_kill import solar_kill
 from logger import configure_logging
 from user_kill import user_input_kill
 from set_chargings import set_charging
 from progress_bar import run_progress_bar
+from modbus_data import return_data_to_script as get_pv_power
 from wait_for_changes import wait_for_amp_changes
 
-def check_1_phase(first_check: bool = False, use_three_phases: bool = False) -> bool:
+def check_1_phase(first_check: bool = False) -> bool:
     """Checks if one phase is being used for charging.
 
     Args:
         first_check (bool): Flag to indicate if this is the first check.
             Defaults to False.
-        use_three_phases (bool): Flag to indicate if the systems has enough solar power for three phase charging
-            Default to False.
 
     Returns:
         bool: Returns True if one phase is being used for charging.
@@ -24,27 +24,27 @@ def check_1_phase(first_check: bool = False, use_three_phases: bool = False) -> 
     check_1_phase_url = "http://192.168.2.203/api/status?filter=fsp"
 
     while True:
+        pv_power, _, _, _ = get_pv_power()
+        if datetime.now().hour == 20:
+            solar_kill(pv_power)
         # Get the value of fsp from the API
         fsp = get(check_1_phase_url).json()['fsp']
         if fsp:
             if first_check:
-                # displaying progress bar, only for playing around and why not?!
-                run_progress_bar()
-
                 # configuring logging
                 configure_logging()
+                # displaying progress bar, only for playing around and why not?!
+                run_progress_bar()
             return True
         if not fsp:
-            if use_three_phases:
-                return True
             # If this is the first check and three phases are being used, wait for one phase to be used
             if wait_for_amp_changes():
                 # If amps have changed three times indicate the user the pv-charging will stop and start charging
                 user_input_kill()
-                sys.exit()  ################### replace with suitable wait loop for changes in phases or something like this ###################
+                while not get(check_1_phase_url).json()['fsp']:
+                    sleep(10)
             # if amps hasnt chanegd three times, stop charging and wait for one phase to be used
             set_charging(1)
-        sleep(30)
 
 
 if __name__ == '__main__':
