ad0df0af0e987615d4aeea1be6bd2fee07aa356e
Duncan1106 <duncan.zirkel@gmail.com>
2023-11-30 22:04:41 +0100

    added possibility for user to switch off the script and charge normally

--- wait_for_changes.py
+++ wait_for_changes.py
@@ -1,24 +1,23 @@
-from requests import get, json
+import requests
 from time import sleep, time
-
 def wait_for_amp_changes():
     url = "http://192.168.2.203/api/status?filter=amp"
-    response = get(url)
-    current_value = json()['amp']
+    response = requests.get(url)
+    current_value = response.json()['amp']
     num_changes = 0
     start_time = time()
 
-    while num_changes < 3 and time() - start_time < 30:
-        sleep(0.25)  # wait for .25 seconds
+    while num_changes < 2 and time() - start_time < 30:
+        sleep(0.5)  # wait for .5 seconds
         response = requests.get(url)
         new_value = response.json()['amp']
 
         if new_value != current_value:
             num_changes += 1
-            print(f"Value changed ({num_changes}/3): {new_value}")
+            print(f"Value changed ({num_changes}/2): {new_value}")
             current_value = new_value
 
-    if num_changes ´== 3:
+    if num_changes == 2:
         return True
     else:
         return False
