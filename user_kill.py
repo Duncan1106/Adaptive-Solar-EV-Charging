b018b6f102c54041763ca48830898a29d6f0a98c
Duncan1106 <duncan.zirkel@gmail.com>
2023-11-30 22:02:31 +0100

    updated user kill

---
+++ user_kill.py
@@ -0,0 +1,15 @@
+from time import sleep
+from set_amps import set_amp
+
+
+# user input signal that indicates script wont interrupt anymore
+def user_input_kill() -> None:
+    """
+    This function sets the charging amps to two different values to indicate to the user that input was received.
+    """
+    set_amp(6)
+    sleep(2)
+    set_amp(10)
+    sleep(2)
+    set_amp(16)
+    sleep(2)
